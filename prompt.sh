cd3dab3942df7b755b0c5ae38665a126af23a175
Alan Barnett <alanbarnett328@gmail.com>
2019-03-07 02:36:08 -0800

    Add function for opening theme in editor, organize

--- prompt.sh
+++ prompt.sh
@@ -4,12 +4,18 @@ export PROMPT_THEME="twoline"
 # Path of base directory (used in theme scripts to find bin folder)
 export PROMPT_BASE_DIR="$HOME/bin/bash_prompt"
 
-# How many directories to display before truncating
-PROMPT_DIRTRIM=8
-
 prompt_command ()
 {
     PS1=$("$PROMPT_BASE_DIR"/themes/"$PROMPT_THEME" "$?")
 }
 
 PROMPT_COMMAND=prompt_command
+
+# How many directories to display before truncating
+PROMPT_DIRTRIM=8
+
+# Function to open the current theme in your editor
+ept ()
+{
+	"$EDITOR" "$PROMPT_BASE_DIR"/themes/"$PROMPT_THEME"
+}
