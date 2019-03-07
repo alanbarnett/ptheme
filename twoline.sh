9f6474af88172178418c27177de8164d915bca78
Alan Barnett <alanbarnett328@gmail.com>
2019-03-06 22:39:22 -0800

    Change ps1.sh to twoline.sh
    New scheme is the names are themes

---
+++ twoline.sh
@@ -0,0 +1,28 @@
+##PS1='\[\e[1;37m\]\342\224\214[\[\e[1;36m\]\u$(network_status.sh)\[\e[1;36m\]\h\[\e[1;37m\]]\342\224\200(\[\e[1;32m\]\w$(git_branch_prompt.sh)\[\e[1;37m\])\342\224\200($(battery)\[\e[1;37m\])\342\224\200(\[\e[1;34m\]$(date "+%I:%M %P")\[\e[1;37m\])$(need_to_reboot.sh)\n\[\e[1;37m\]\342\224\224\342\224\200(\[\e[1;36m\]$\[\e[1;37m\]> \[\e[m\]'
+#
+# Variable for control of git information in my prompt
+#export GIT_PROMPT=1
+
+# Update path to include folder with necessary prompt scripts
+#PATH=$PATH:$HOME/bin/bash_prompt
+
+# Color shortcut variables
+RED='\[\e[1;31m\]'
+GREEN='\[\e[1;32m\]'
+BLUE='\[\e[1;34m\]'
+CYAN='\[\e[1;36m\]'
+WHITE='\[\e[1;37m\]'
+NORM='\[\e[m\]'
+
+# Box printing shortcut variables
+BOX_TOPL='\342\224\214'
+BOX_BOTL='\342\224\224'
+BOX_HORIZ='\342\224\200'
+
+DATE_SEQ="+%I:%M %P"
+
+echo "${WHITE}${BOX_TOPL}[${CYAN}\u\
+$(network_status.sh)\
+${CYAN}\h${WHITE}]\
+${BOX_HORIZ}(${GREEN}\w$(git_prompt.sh)${WHITE})\
+\n${WHITE}${BOX_BOTL}(${CYAN}\$${WHITE}> ${NORM}"
