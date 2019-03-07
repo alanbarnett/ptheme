876bbddbee04b9d9d22f3f0132dc1ecbf3499e02
Alan Barnett <alanbarnett328@gmail.com>
2019-03-07 04:27:45 -0800

    Move additional config out, make this the default

--- prompt.sh
+++ prompt.sh
@@ -1,10 +1,10 @@
 # Default prompt theme
-export PROMPT_THEME="twoline"
+export PROMPT_THEME="default"
 
 # Path of base directory (used in theme scripts to find bin folder)
 export PROMPT_BASE_DIR="$HOME/bin/bash_prompt"
 
-# Path of prompt theme directory (best to put it in one place)
+# Path of prompt theme directory (used to avoid rewriting the path)
 export PROMPT_THEME_DIR="$PROMPT_BASE_DIR/themes"
 
 prompt_command ()
@@ -13,29 +13,3 @@ prompt_command ()
 }
 
 PROMPT_COMMAND=prompt_command
-
-# How many directories to display before truncating
-PROMPT_DIRTRIM=8
-
-# Function to open the current theme in your editor
-ept ()
-{
-	"$EDITOR" "$PROMPT_THEME_DIR"/"$PROMPT_THEME"
-}
-
-# Function to quickly switch themes
-# No args: display current theme
-# One arg: check if theme exists, switch to it if so, print error if no
-# Multiple args: ignored. you must use "" if you want a space in the name
-pt ()
-{
-	if [ -z "$1" ]; then
-		echo "$PROMPT_THEME"
-	else
-		if [ -f "$PROMPT_THEME_DIR/$1" ]; then
-			PROMPT_THEME="$1"
-		else
-			echo "Theme not in $PROMPT_THEME_DIR"
-		fi
-	fi
-}
