96122b7ee51f8e0abe39765c2e68c50efbdd1407
Alan Barnett <alanbarnett328@gmail.com>
2019-03-07 02:23:38 -0800

    Change prompt command function to fit my style

--- prompt.sh
+++ prompt.sh
@@ -4,9 +4,12 @@ export PROMPT_THEME="twoline"
 # Path of base directory (used in theme scripts to find bin folder)
 export PROMPT_BASE_DIR="$HOME/bin/bash_prompt"
 
-function prompt_command {
-    RET="$?"
-    PS1=$("$PROMPT_BASE_DIR"/themes/"$PROMPT_THEME" "$RET")
-}
+# How many directories to display before truncating
 PROMPT_DIRTRIM=8
+
+prompt_command ()
+{
+    PS1=$("$PROMPT_BASE_DIR"/themes/"$PROMPT_THEME" "$?")
+}
+
 PROMPT_COMMAND=prompt_command
