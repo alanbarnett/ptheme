f0186811b5dc8d0034d17aaf1af484d35459b13d
Alan Barnett <alanbarnett328@gmail.com>
2019-08-24 22:49:07 -0700

    feat(utils.sh): add inputrc config
    Adds helper function to switch prompts, since now I'm also sourcing some
    inputrc files. Best to group all the related things for switching a
    theme into one helper function, to avoid duplicating code. Also removes
    error message printing in _check_theme_exists, to make it as simple as
    possible.

--- plugins/utils.sh
+++ plugins/utils.sh
@@ -13,21 +13,40 @@ _check_theme_exists()
 	if [ -f "$PROMPT_THEME_DIR/$1" ]; then
 		return 0
 	else
-		echo "Theme \"$1\" not in $PROMPT_THEME_DIR"
 		return 1
 	fi
+}
+
+# Helper function for switching themes
+# Changes the variable for the theme to be ran, and sources some .inputrc files
+# Your default one with main configuration, and one for just the theme
+# Useful if you have lots of common settings, and some that change regularly
+_switch_theme()
+{
+	PROMPT_THEME="$1"
+	[ -f "$HOME/.inputrc" ] &&
+		bind -f "$HOME/.inputrc"
+	[ -f "$PROMPT_THEME_DIR/.$PROMPT_THEME.inputrc" ] &&
+		bind -f "$PROMPT_THEME_DIR/.$PROMPT_THEME.inputrc"
+	return 0
 }
 
 # Function to quickly switch themes
 # No args: display current theme
 # One arg: check if theme exists, switch to it if so, print error if no
+#			also run that theme's inputrc file
 # Multiple args: ignored. you must use "" if you want a space in the name
 pt ()
 {
 	if [ -z "$1" ]; then
 		echo "$PROMPT_THEME"
 	else
-		_check_theme_exists "$1" && PROMPT_THEME="$1"
+		if _check_theme_exists "$1"; then
+			_switch_theme "$1"
+		else
+			echo "Theme \"$1\" not in $PROMPT_THEME_DIR"
+			return 1
+		fi
 	fi
 }
 
