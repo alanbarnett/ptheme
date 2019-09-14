858139a45198df1502291ad32465fb25f6b64d26
Alan Barnett <alanbarnett328@gmail.com>
2019-09-14 16:20:16 -0700

    refactor(git_prompt): echo variables with git info
    Change from echoing a created prompt string to echoing variables that
    can be used to create that prompt string. It takes arguments for the
    things you want to gather, so that it doesn't do things you won't need.

--- bin/git_prompt.sh
+++ bin/git_prompt.sh
@@ -1,31 +1,78 @@
 #!/bin/sh
 
-COLOR_GOOD='\e[0;32m'
-COLOR_UNTRACKED='\e[1;33m'
-COLOR_BAD='\e[1;31m'
+function show_usage()
+{
+	echo "########## git status script ##########"
+	echo "Requires the ENABLE_GIT_STATUS variable to be set to 1"
+	echo "Also needs options to do anything"
+	echo ""
+	echo " Options:"
+	echo "	-b|--branch"
+	echo "		echo GIT_BRANCH variable"
+	echo "	-c|--color"
+	echo "		echo GIT_BRANCH and GIT_COLOR variables"
+	echo "		Uses these color variables:"
+	echo "			GIT_COLOR_GOOD: No untracked files and working directory clean"
+	echo "			GIT_COLOR_UNTRACKED: Untracked files, but no unstaged changes"
+	echo "			GIT_COLOR_BAD: Unstaged changes to files"
+	echo "########## ################# ##########"
+}
 
-if [ "$GIT_PROMPT" = "1" ]; then
+# Make sure there are arguments, exit if not
+[ "$#" -lt 1 ] && show_usage 1>&2 && exit
+
+while [ -n "$1" ]; do
+	case "$1" in
+		-b|--branch)
+			GET_BRANCH="1"
+			shift
+			;;
+		-c|--color)
+			GET_BRANCH="1"
+			GET_COLOR="1"
+			shift
+			;;
+		*)
+			show_usage 1>&2
+			exit
+			;;
+	esac
+done
+
+if [ "$ENABLE_GIT_STATUS" = "1" ]; then
 	STATUS=$(git status -sb 2> /dev/null)
 
 	if [ -n "$STATUS" ]; then
-		NUM_STATUS=$(echo "$STATUS" | wc -l)
-		BRANCH=$(echo "$STATUS" | sed "s/\.\.\./\n/" | sed -n "s/^## \(.*\)$/\1/p")
-		UNTRACKED=$(echo "$STATUS" | grep "^?? ")
+		if [ -n "$GET_BRANCH" ]; then
+			BRANCH=$(echo "$STATUS" | sed -e "s/\.\.\./ /" -ne "s/^## //p")
 
-		if [ -n "$UNTRACKED" ]; then
-			NUM_UNTRACKED=$(echo "$UNTRACKED" | wc -l)
-		else
-			NUM_UNTRACKED=0
+			echo "GIT_BRANCH=\"$BRANCH\""
 		fi
 
-		NUM_OTHER=$(( $NUM_STATUS - $NUM_UNTRACKED - 1 ))
+		if [ -n "$GET_COLOR" ]; then
+			# Don't override variable if it's already set, but provide defaults
+			[ -z "$GIT_COLOR_GOOD" ] && GIT_COLOR_GOOD="\e[0;32m"
+			[ -z "$GIT_COLOR_UNTRACKED" ] && GIT_COLOR_UNTRACKED="\e[1;33m"
+			[ -z "$GIT_COLOR_BAD" ] && GIT_COLOR_BAD="\e[1;31m"
 
-		if [ $NUM_OTHER -gt 0 ]; then
-			printf "\e[1;37m->$COLOR_BAD$BRANCH\e[m"
-		elif [ $NUM_UNTRACKED -gt 0 ]; then
-			printf "\e[1;37m->$COLOR_UNTRACKED$BRANCH\e[m"
-		else
-			printf "\e[1;37m->$COLOR_GOOD$BRANCH\e[m"
+			NUM_STATUS=$(echo "$STATUS" | wc -l)
+			UNTRACKED=$(echo "$STATUS" | grep "^?? ")
+
+			if [ -n "$UNTRACKED" ]; then
+				NUM_UNTRACKED=$(echo "$UNTRACKED" | wc -l)
+			else
+				NUM_UNTRACKED=0
+			fi
+
+			NUM_OTHER=$(( $NUM_STATUS - $NUM_UNTRACKED - 1 ))
+
+			if [ $NUM_OTHER -gt 0 ]; then
+				echo "GIT_COLOR=\"$GIT_COLOR_BAD\""
+			elif [ $NUM_UNTRACKED -gt 0 ]; then
+				echo "GIT_COLOR=\"$GIT_COLOR_UNTRACKED\""
+			else
+				echo "GIT_COLOR=\"$GIT_COLOR_GOOD\""
+			fi
 		fi
 	fi
 fi
