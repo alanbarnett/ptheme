f72426e466af256b28272b2304078779407c9d2d
Alan Barnett <alanbarnett328@gmail.com>
2019-10-12 21:48:38 -0700

    fix(check_kernel_behind): exit correctly
    Exit 1 instead of 0 if we can't find pacman, to indicate that the kernel
    is not behind (more like we can't check because I don't have a reason to
    make this more portable yet). Also looks for pacman with /usr/bin/pacman
    instead of which pacman, to avoid other programs appearing under which.

--- bin/check_kernel_behind.sh
+++ bin/check_kernel_behind.sh
@@ -2,7 +2,7 @@
 
 # Exit if we don't have pacman
 # TODO make this script more portable, but for now it doesn't matter
-[ -z "$(which pacman)" ] && exit
+[ ! -x "/usr/bin/pacman" ] && exit 1
 
 # If the version of the linux kernel installed via pacman is newer than the one
 # you're using according to `uname -r`, then this script will exit
@@ -11,7 +11,7 @@
 # updating.
 
 UNAME=$(uname -r)
-KERNEL=$(pacman -Q linux | tr _ - | awk -F ' ' '{ print $2 }' -)
+KERNEL=$(/usr/bin/pacman -Q linux | tr _ - | awk -F ' ' '{ print $2 }' -)
 
 # Exit with an error if they are the same
 # This way, the script fails "if" checks if your running kernel is fine
