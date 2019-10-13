700c64f36524b6d824492c0210c925a2c6e2579e
Alan Barnett <alanbarnett328@gmail.com>
2019-10-12 18:49:33 -0700

    update(battery_percent_color): use gradient
    Change the battery_percent_color script to color the battery with an
    11-step gradient of the xterm 256 colors. It's also overwritable from
    the theme file, as well as the color for charging. This also sets the
    local script variables to have lowercase names, because that's better
    style.

--- bin/battery_percent_color.sh
+++ bin/battery_percent_color.sh
@@ -3,34 +3,30 @@
 # TODO make better usage message
 function show_usage()
 {
-	echo "Takes the current battery percent as the first argument." 1>&2
+	echo "usage: battery_percent_color.sh battery_percent
+Takes the current battery percent as the first argument.
+Prints the color it should be." 1>&2
 }
 
 # Quit script if we don't have exactly one argument
 [ "$#" != "1" ] && show_usage && exit
 
 # Set the file with the battery status, and quit if it doesn't exist
-STATUS_FILE="/sys/class/power_supply/BAT0/status"
-[ ! -f "$STATUS_FILE" ] && exit
+status_file="/sys/class/power_supply/BAT0/status"
+[ ! -f "$status_file" ] && exit
 
-PERCENT="$1"
-STATUS="$(cat "$STATUS_FILE")"
+battery_percent="$1"
+battery_status="$(cat "$status_file")"
 
 # Define default color variables, if they aren't already set
-[ -z "$BAT_PROMPT_COLOR_CHARGING" ] && BAT_PROMPT_COLOR_CHARGING="\e[1;35m"
-[ -z "$BAT_PROMPT_COLOR_DYING" ] && BAT_PROMPT_COLOR_DYING="\e[1;5;31m"
-[ -z "$BAT_PROMPT_COLOR_LOW" ] && BAT_PROMPT_COLOR_LOW="\e[1;31m"
-[ -z "$BAT_PROMPT_COLOR_OKAY" ] && BAT_PROMPT_COLOR_OKAY="\e[1;33m"
-[ -z "$BAT_PROMPT_COLOR_GOOD" ] && BAT_PROMPT_COLOR_GOOD="\e[1;32m"
+[ -z "$BATTERY_COLORS" ] && \
+	BATTERY_COLORS=(124 160 196 202 208 214 220 184 148 112 76 40)
+[ -z "$BATTERY_CHARGING_COLOR" ] && BATTERY_CHARGING_COLOR="201"
 
-if [ "$STATUS" == "Charging" ] || [ "$STATUS" == "Unknown" ]; then
-	echo "$BAT_PROMPT_COLOR_CHARGING"
-elif [ "$PERCENT" -lt "11" ]; then
-	echo "$BAT_PROMPT_COLOR_DYING"
-elif [ "$PERCENT" -lt "26" ]; then
-	echo "$BAT_PROMPT_COLOR_LOW"
-elif [ "$PERCENT" -lt "80" ]; then
-	echo "$BAT_PROMPT_COLOR_OKAY"
+if [ "$battery_status" == "Charging" ] || [ "$battery_status" == "Unknown" ]; then
+	color="$BATTERY_CHARGING_COLOR"
 else
-	echo "$BAT_PROMPT_COLOR_GOOD"
+	color="${BATTERY_COLORS[$(( $battery_percent / 10 ))]}"
 fi
+
+echo "\e[38;5;${color}m"
